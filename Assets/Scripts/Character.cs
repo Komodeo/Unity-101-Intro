139a63bf620fcc640f3714b8b53625b176da4399
Komodeo <onlythebest@gmail.com>
2020-06-03 14:37:04 -0400

    lesson 9

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,7 +7,29 @@ public class Character : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello world!");
+        //declare
+        string characterName;
+
+        //assign
+        characterName = "Grant"
+
+        int characterLevel = 10;
+        int experience = characterLevel * 5;
+
+        float itemDurability = 1.567f;
+
+        experience = (int)itemDurability;
+
+        characterName = itemDurability.ToString();
+
+        
+
+        bool equippable = false;
+        if (itemDurability > 1f)
+        {
+            experience = (int)(itemDurability * 1.5f) / characterLevel;
+        }
+
     }
 
 }
